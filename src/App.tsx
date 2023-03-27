375cac3a63539012b3e317c1521d0a923e7ca109
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-27 12:16:58 -0300

    Refactor App component to include Header and SummaryTable components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './styles/global.css'
-import { Habit } from "./components/Habit"
+import { Header } from './components/Header'
+import { SumaryTable } from './components/SumaryTable'
+// import { Habit } from "./components/Habit"
 
-function App() {
+export function App() {
   return (
-    <Habit completed={5} />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <SumaryTable />
+      </div>
+    </div>
   )
 }
-
-export default App
